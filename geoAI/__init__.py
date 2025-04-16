5ca03af6ee5af4a1a520f0c2eb1293543ee7d6ba
Songyan Zhu <zhusy93@gmail.com>
2025-04-16 09:57:57 +0100

    Update __init__.py

--- geoAI/__init__.py
+++ geoAI/__init__.py
@@ -1,8 +1,11 @@
 import os
+from pathlib import Path
 from . import functions as gfuncs
 
-__all__ = ["gfuncs", "test_path"]
+__all__ = ["gfuncs", "data_path"]
 
-_module_path = os.path.dirname(__file__)
+# _module_path = os.path.dirname(__file__)
 
-test_path = _module_path
+# test_path = _module_path
+
+data_path = Path(__file__).parent.joinpath('data')
